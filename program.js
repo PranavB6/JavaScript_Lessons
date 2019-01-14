fc4c3ffb33e2c228cd57440e92a9fed4248dc94f
PranavB6 <b.pranav.k@gmail.com>
2019-01-14 15:24:26 -0700

    Understanding JavaScript objects vs Python Dictionaries

--- program.js
+++ program.js
@@ -2,18 +2,65 @@ document.writeln('Hello, world!');
 
 console.log('Program start');
 
-let array = [[1, 2, 3], [4, 5], [6]];
+class Rabbit {
+    constructor(type) {
+        this.type = type;
+    }
 
-let new_array = array.reduce((total_array, subarray) => total_array.concat(subarray));
-
-console.log(array);
-console.log(new_array);
-
-function printLst(lst) {
-    curr = lst;
-
-    while(curr != null) {
-        console.log(curr.value);
-        curr = curr.rest;
+    speak(line) {
+        console.log(`The ${this.type} rabbit says ${line}`);
     }
 }
+
+let killerRabbit = new Rabbit("Killer");
+let blackRabbit = new Rabbit("Black");
+
+killerRabbit.speak("I'll kill you");
+blackRabbit.speak("Hi Everybody");
+
+killerRabbit.full = "No";
+
+console.log(killerRabbit.full);
+
+let obj = {};
+
+let a = "abc";
+
+obj[a] = "property a";
+
+console.log(obj[a]);
+console.log(obj.a);
+console.log(obj["a"]);
+console.log(obj["abc"]);
+
+console.log("-------");
+
+obj = {a: "propert a obj 2"};
+
+console.log(obj[a]);
+console.log(obj.a);
+console.log(obj["a"]);
+console.log(obj["abc"]);
+
+// let array = [1, 2, 3, 4, 5, 6];
+
+// function every(array, test) {
+//     for (let element of array) {
+//         if (!test(element))
+//             return false;
+//     }
+
+//     return true;
+// }
+
+// console.log(every(array, n => n < 7));
+// console.log(every([], n => n < 10));
+
+// function printLst(lst) {
+//     curr = lst;
+
+//     while(curr != null) {
+//         console.log(curr.value);
+//         curr = curr.rest;
+//     }
+// }

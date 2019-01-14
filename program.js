5fb3510b53cf8f9fee7efa7d098ff82a7df72f9c
PranavB6 <b.pranav.k@gmail.com>
2019-01-14 12:03:16 -0700

    Finished flattening function

--- program.js
+++ program.js
@@ -2,66 +2,14 @@ document.writeln('Hello, world!');
 
 console.log('Program start');
 
-let array = [0, 1, 2, 3, 4, 5];
-// let lst = {value: null};
+let array = [[1, 2, 3], [4, 5], [6]];
 
-// function arrayToList(array) {
-//     if (array.length == 1) {
-//         let lst = {
-//             value: array[0],
-//             rest: null
-//         };
+let new_array = array.reduce((total_array, subarray) => total_array.concat(subarray));
 
-//         return lst;
-//     }
-
-//     let lst = {
-//         value: array[0],
-//         rest: arrayToList(array.slice(1, ))
-//     }
-
-//     return lst;
-// }
-
-function arrayToList(array) {
-
-    let tail = null;
-    let lst = null;
-
-    for (let i = array.length-1; i >= 0; --i) {
-        lst = {
-            value: array[i],
-            rest: tail
-        };
-
-        tail = lst;
-    }
-
-    return tail;
-}
-
-function listToArray(list) {
-
-    array = [];
-    curr = list;
-
-    while (curr != null) {
-        array.push(curr.value)
-        curr = curr.rest;
-    }
-
-    return array;
-}
-
-
-console.log(listToArray(arrayToList(array)));
-
-console.log(array.entries());
-
-// printLst(lst);
+console.log(array);
+console.log(new_array);
 
 function printLst(lst) {
-
     curr = lst;
 
     while(curr != null) {

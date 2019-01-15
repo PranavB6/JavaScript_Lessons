2d40a53b8162e8e4487817e69bd820163e686f3a
PranavB6 <b.pranav.k@gmail.com>
2019-01-15 09:51:40 -0700

    Group Class Exercise

--- program.js
+++ program.js
@@ -2,41 +2,129 @@ document.writeln('Hello, world!');
 
 console.log('Program start');
 
-class Vec {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+class Group {
+    constructor() {
+        this.group = [];
     }
 
-    plus(other) {
-        return new Vec(this.x + other.x, this.y + other.y);
+    add(value) {
+        if (!this.has(value)) {
+            this.group.push(value);
+
+        } else {
+            console.log("Already in group");
+        }
+
+        
     }
 
-    minus(other) {
-        return new Vec(this.x - other.x, this.y - other.y);
+    delete(value) {
+        let idx = this.group.indexOf(value);
+
+        if (idx != -1) {
+            this.group.splice(idx, 1);
+
+        } else {
+            console.log(`${value} is not in the group`);
+        }
     }
 
-    get length() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    has(value) {
+        return (this.group.indexOf(value) != -1);
     }
 
-    set length(value) {
-        this.x = 5;
-        this.y = 12;
-        console.log(`value: ${value}`);
-        console.log("x = 5, y = 12");
+    static from(objects) {
+
+        let group = new Group();
+
+        for (let object of objects) {
+            
+            group.add(object);
+        }
+
+        return group;
     }
 }
+
+let group = Group.from([10, 20]);
+console.log("---");
+
+console.log(
+    group.has(10),
+    group.has(30),
+    group.add(10),
+    group.delete(10),
+    group.has(10)
+)
+
+
+
+// print(
+//     group.has(10),
+//     group.has("10")
+// )
+
+// group.add("10");
+
+// print( group.has(10) );
+
+// group.delete(10);
+
+
+// print( group.has('10') );
+
+
+
+
+
+
+
+
+
+
+
+function print(...objs) {
+    console.log(objs);
+    for (let obj of objs) {
+        console.log(obj);
+    }
+}
+
+// class Vec {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+
+//     plus(other) {
+//         return new Vec(this.x + other.x, this.y + other.y);
+//     }
+
+//     minus(other) {
+//         return new Vec(this.x - other.x, this.y - other.y);
+//     }
+
+//     get length() {
+//         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+//     }
+
+//     set length(value) {
+//         this.x = 5;
+//         this.y = 12;
+//         console.log(`value: ${value}`);
+//         console.log("x = 5, y = 12");
+//     }
+// }
 
 // console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 
 // console.log(new Vec(3, 4).length);
 
-let vec = new Vec(3, 4);
-console.log("Length", vec.length);
-vec.length = [[6, 7], ["wfq"]];
-console.log("New length", vec.length);
+// let vec = new Vec(3, 4);
+// console.log("Length", vec.length);
+// vec.length = [[6, 7], ["wfq"]];
+// console.log("New length", vec.length);
 
 // class Rabbit {
 //     constructor(type) {
